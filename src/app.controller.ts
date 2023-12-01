24148706a3824a51dacbc7256a43dba7add2a10d
Leonardo Figueiredo <leonardo.figueiredo@ipflagship.com.br>
2023-12-01 16:52:50 -0300

    add authorization

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Roles } from './auth/roles.decorator';
+import { RolesGuard } from './auth/roles.guard';
+import { Role } from './auth/enums/role.enum';
 
 @Controller()
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Roles(Role.Teste)
   getHello(@Req() req): string {
     return this.appService.getHello(req.user.preferred_username);
   }
